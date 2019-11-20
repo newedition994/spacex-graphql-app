1fbf72912664122b44fc09b99c8f5f85040ae8b8
Breyanna Vass <breyannavass@gmail.com>
2019-11-19 19:45:59 -0500

    calling on react appollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 import logo from "./logo.png";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <img src={logo} alt="" />
-    </div>
-  );
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql"
+});
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div className="container">
+          <img
+            src={logo}
+            alt="SpaceX"
+            style={{ width: 300, display: "block", margin: "auto" }}
+          />
+        </div>
+      </ApolloProvider>
+    );
+  }
 }
 
 export default App;
